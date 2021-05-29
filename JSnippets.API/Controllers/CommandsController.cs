cc56a0240e7ee7b99dafc15f5c167a01ef46abeb
jmayorga94 <javi.mayorga1994@gmail.com>
2021-05-28 23:44:25 -0600

    Initial Commit

---
+++ JSnippets.API/Controllers/CommandsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSnippets.API.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "John", "Paul", "George" };
+
+        }
+    }
+}
